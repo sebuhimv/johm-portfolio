e3536be96e41c59c7a62798f4959752a03bad329
SebuhiMv <sebuhimv@gmail.com>
2023-02-08 02:43:32 +0400

    Navbar yaradildi ve bracha elave edildi

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+    return (
+
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="#">Work</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Blog</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar
